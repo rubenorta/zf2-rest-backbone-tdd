7ecd771f2e105c2c1a6a01fdbfefb3f65f3739ce
Alfonso Martín <alfonsomartin@gmail.com>
2014-11-12 17:29:29 +0100

    View Gastos Item: la vista renderiza bien el template si le pasamos un modelo correcto. (green)

--- public/js/views/gastos.js
+++ public/js/views/gastos.js
@@ -2,13 +2,27 @@ $(function(){
 
     APP.namespace( "APP.views.Gastos.Item" );
     APP.views.Gastos.Item = Backbone.View.extend({
+        
         model: null,
+        tagName: 'tr',
+        className: 'gasto',
+        
+        template: _.template(
+            '<td class="cantidad"><%= cantidad %></td>'+
+            '<td class="descripcion"><%= descripcion %></td>'
+        ),
+        
         initialize: function(){
             if ( !this.model ) {
                 throw new Error( "Es necesario un modelo" );
             }
             this.model.view = this;
         },
+
+        render: function(){
+            this.$el.html( this.template( this.model.attributes ) );
+            return this;
+        }
     });
 
     APP.namespace( "APP.views.Gastos.Listado" );
