cfb3d7ff15a05f3a7c861e43ffd1443f79b5114f
Alfonso Martín <alfonsomartin@gmail.com>
2014-11-12 12:05:02 +0100

    * Test APP.namespace (red)

--- test/spec/gastosSpec.js
+++ test/spec/gastosSpec.js
@@ -1,5 +1,11 @@
-describe("APP", function() {
+describe("APP existe en global scope", function() {
     it('Namespace definido', function() {
         expect(APP).toBeDefined();
+    });
+
+    describe("y tiene un método para crear organizar nuestra aplicación", function() {
+        it('APP.namespace está definido', function() {
+            expect(APP.namespace).toBeDefined();
+        });
     });
 });
