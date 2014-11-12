9cd1cdada0297cc7bdd0b9329d0c56802228c40e
Alfonso Martín <alfonsomartin@gmail.com>
2014-11-12 12:31:21 +0100

    Test APP.namespace (green)

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,35 @@
 var APP = APP || {};
+
+(function( app ) {
+
+    "use strict";
+
+    app.global = this;
+
+    /** 
+     * @desc: crea la estructura necesaria para alojar nuestro módulo en la ruta
+     *        especificada.
+     * @param string {String}: la ruta donde vayamos a alojar el módulo.
+     * @return {Object}: el objeto vacío donde podremos meter el módulo.
+     */
+    app.namespace = function ( string ) {
+        var parts = string.split("."),
+            parent = app,
+            i;
+
+        if (parts[0] === "APP") {
+            parts = parts.slice(1);
+        }
+
+        for (i = 0; i < parts.length; i += 1) {
+            if (typeof parent[parts[i]] === "undefined") {
+                parent[parts[i]] = {};
+            }
+            parent = parent[parts[i]];
+        }
+
+        return parent;
+
+    };
+
+}.call( this, APP ));
--- test/spec/gastosSpec.js
+++ test/spec/gastosSpec.js
@@ -3,9 +3,14 @@ describe("APP existe en global scope", function() {
         expect(APP).toBeDefined();
     });
 
-    describe("y tiene un método para crear organizar nuestra aplicación", function() {
+    describe("y tiene un método para organizar nuestra aplicación", function() {
         it('APP.namespace está definido', function() {
             expect(APP.namespace).toBeDefined();
+        });
+        it('y crea objetos vacíos donde almacenar los módulos', function() {
+            APP.namespace( "APP.router" );
+            expect(APP.router).toBeDefined();
+            expect(APP.router).toEqual({});
         });
     });
 });
